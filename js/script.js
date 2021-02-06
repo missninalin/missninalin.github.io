50a4e4d5f718a29bb5cc2bedabb17bfb38553910
missninalin <33407793+missninalin@users.noreply.github.com>
2021-02-05 21:45:42 -0500

    contact form edit

--- js/script.js
+++ js/script.js
@@ -91,88 +91,88 @@ $(function(){
 	/*=========================================================================
 		Contact Form
 	=========================================================================*/
-	function isJSON(val){
-		var str = val.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
-		return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
-	}
-	$('#contact-form').validator().on('submit', function (e) {
+	// function isJSON(val){
+	// 	var str = val.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
+	// 	return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
+	// }
+	// $('#contact-form').validator().on('submit', function (e) {
 		
-		if (!e.isDefaultPrevented()) {
-			// If there is no any error in validation then send the message
+	// 	if (!e.isDefaultPrevented()) {
+	// 		// If there is no any error in validation then send the message
 			
-			e.preventDefault();
-			var $this = $(this),
+	// 		e.preventDefault();
+	// 		var $this = $(this),
 				
-				//You can edit alerts here
-				alerts = {
+	// 			//You can edit alerts here
+	// 			alerts = {
 				
-					success: 
-					"<div class='form-group' >\
-						<div class='alert alert-success alert-dismissible' role='alert'> \
-							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-								<i class='ion-ios-close-empty' ></i> \
-							</button> \
-							<strong>Message Sent!</strong> We'll be in touch as soon as possible\
-						</div>\
-					</div>",
+	// 				success: 
+	// 				"<div class='form-group' >\
+	// 					<div class='alert alert-success alert-dismissible' role='alert'> \
+	// 						<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
+	// 							<i class='ion-ios-close-empty' ></i> \
+	// 						</button> \
+	// 						<strong>Message Sent!</strong> We'll be in touch as soon as possible\
+	// 					</div>\
+	// 				</div>",
 					
 					
-					error: 
-					"<div class='form-group' >\
-						<div class='alert alert-danger alert-dismissible' role='alert'> \
-							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-								<i class='ion-ios-close-empty' ></i> \
-							</button> \
-							<strong>Error!</strong> Sorry, an error occurred. Try again.\
-						</div>\
-					</div>"
+	// 				error: 
+	// 				"<div class='form-group' >\
+	// 					<div class='alert alert-danger alert-dismissible' role='alert'> \
+	// 						<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
+	// 							<i class='ion-ios-close-empty' ></i> \
+	// 						</button> \
+	// 						<strong>Error!</strong> Sorry, an error occurred. Try again.\
+	// 					</div>\
+	// 				</div>"
 					
-				};
+	// 			};
 			
-			$.ajax({
+	// 		$.ajax({
 			
-				url: 'mail.php',
-				type: 'post',
-				data: $this.serialize(),
-				success: function(data){
+	// 			url: 'mail.php',
+	// 			type: 'post',
+	// 			data: $this.serialize(),
+	// 			success: function(data){
 					
-					if( isJSON(data) ){
+	// 				if( isJSON(data) ){
 						
-						data = $.parseJSON(data);
+	// 					data = $.parseJSON(data);
 						
-						if(data['error'] == false){
+	// 					if(data['error'] == false){
 							
-							$('#contact-form-result').html(alerts.success);
+	// 						$('#contact-form-result').html(alerts.success);
 							
-							$('#contact-form').trigger('reset');
+	// 						$('#contact-form').trigger('reset');
 							
-						}else{
+	// 					}else{
 							
-							$('#contact-form-result').html(
-							"<div class='form-group' >\
-								<div class='alert alert-danger alert-dismissible' role='alert'> \
-									<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-										<i class='ion-ios-close-empty' ></i> \
-									</button> \
-									"+ data['error'] +"\
-								</div>\
-							</div>"
-							);
+	// 						$('#contact-form-result').html(
+	// 						"<div class='form-group' >\
+	// 							<div class='alert alert-danger alert-dismissible' role='alert'> \
+	// 								<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
+	// 									<i class='ion-ios-close-empty' ></i> \
+	// 								</button> \
+	// 								"+ data['error'] +"\
+	// 							</div>\
+	// 						</div>"
+	// 						);
 							
-						}
+	// 					}
 						
 						
-					}else{
-						$('#contact-form-result').html(alerts.error);
-					}
+	// 				}else{
+	// 					$('#contact-form-result').html(alerts.error);
+	// 				}
 					
-				},
-				error: function(){
-					$('#contact-form-result').html(alerts.error);
-				}
-			});
-		}
-	});
+	// 			},
+	// 			error: function(){
+	// 				$('#contact-form-result').html(alerts.error);
+	// 			}
+	// 		});
+	// 	}
+	// });
 	
 	
 });
